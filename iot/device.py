20fcdecce954bddec092bd20974c0243387066b8
Raven <krzysztof.ekiert@gmail.com>
2022-04-26 18:38:01 +0200

    device

---
+++ iot/device.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+from message import MessageType
+
+class Device:
+    def __init__(self, type):
+        self.type = type
+
+    @abstractmethod
+    def connect(self):
+        return None
+
+    @abstractmethod
+    def disconnect(self):
+        return None
+
+    @abstractmethod
+    def send_message(self, mt: MessageType, data: str):
+        return None
+
+    @abstractmethod
+    def status_update(self) -> str:
+        return "Status"
